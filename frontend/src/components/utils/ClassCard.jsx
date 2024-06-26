bbaf8a9fa5b96e70fd442f9428324152f6a6cd73
Kashish Goyal <84564789+KashishGoyal905@users.noreply.github.com>
2024-06-26 15:35:34 +0530

    Added Styles on Cards & Fixed the overall Styles of Comp

--- frontend/src/components/utils/ClassCard.jsx
+++ frontend/src/components/utils/ClassCard.jsx
@@ -1,16 +1,19 @@
 // src/components/ClassCard.js
 export default function ClassCard({ image, title, description, time, instructor }) {
     return (
-        <div className="card glass w-full md:w-96 h-[34rem] mt-6 md:mt-0">
-            <figure>
-                <img src={image} alt={title} />
+        <div className="card glass my-4 md:my-0 w-full md:w-96 h-[34rem] rounded-lg shadow-lg overflow-hidden transform transition-transform hover:scale-105">
+            <figure className="w-full h-64 overflow-hidden">
+                <img src={image} alt={title} className="w-full h-full object-cover" />
             </figure>
-            <div className="card-body">
-                <h2 className="text-xl md:text-2xl font-bold text-center">{title}</h2>
-                <p className="text-center">{description}</p>
-                <p className="text-center font-semibold">Time: {time}</p>
-                <p className="text-center">Instructor: {instructor}</p>
-                <div className="card-actions justify-end">
+            <div className="card-body p-4">
+                <h2 className="text-xl md:text-2xl font-bold text-center mb-2">{title}</h2>
+                <p className="text-center mb-4">{description}</p>
+                <p className="text-center font-semibold mb-2">Time: {time}</p>
+                <p className="text-center mb-4">Instructor: {instructor}</p>
+                <div className="card-actions justify-between">
+                    <div>
+                        <h2 className="mt-2 text-2xl font-bold text-left text-green-400">$40</h2>
+                    </div>
                     <button className="btn btn-primary">Join Now</button>
                 </div>
             </div>
